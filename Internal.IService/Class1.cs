75bf72f93fcb053ec31f15454ab661f681a12e85
1961117443 <1961117443@qq.com>
2019-06-09 15:59:13 +0800

    添加项目文件。

---
+++ Internal.IService/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Internal.IService
+{
+    public class Class1
+    {
+    }
+}
---
+++ Internal.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Internal.Service
+{
+    public class Class1
+    {
+    }
+}
