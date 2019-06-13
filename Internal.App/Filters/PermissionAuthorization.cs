e7562d9303d5d014c6140f4d0373d52754f73949
1961117443 <1961117443@qq.com>
2019-06-13 18:11:02 +0800

    搭建授权认证

---
+++ Internal.App/Filters/PermissionAuthorization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Internal.App.Filters
+{
+    public class PermissionRequirement : IAuthorizationRequirement
+    {
+
+    }
+    public class PermissionAuthorizationHandler : AuthorizationHandler<PermissionRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionRequirement requirement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
